0d663e0d9e0aacdca90ee14494e2b400185daeb9
Logtoua <tlor14@csuchico.edu>
2026-09-02 13:20:52 -0700

    fix: not a number

--- main.cpp
+++ main.cpp
@@ -9,13 +9,17 @@ int main()
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " << x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+
+    if ((x != 0) || (y != 0)) {
+        std::cout << "Multiplication: " << x * y << std::endl;
+        std::cout << "Division: " << x / y << std::endl;
+        std::cout << "Remainder: " << x % y << std::endl;
+        std::cout << "Square Root: " << sqrt(x) << std::endl;
+    } else {
+        std::cout << "Addition: " << x + y << std::endl;
+        std::cout << "Subtraction: " << x - y << std::endl;
+        std::cout << "Square: " << pow(x, y) << std::endl;
+    }
 
     return 0;
 }
